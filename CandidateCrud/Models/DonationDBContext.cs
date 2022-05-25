5da2206577b8472c38240d884f211a08f8d6551f
LeoAdemi <Admin@LEO>
2022-05-25 20:23:09 +0200

    l.a

---
+++ CandidateCrud/Models/DonationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateCrud.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+
+}
